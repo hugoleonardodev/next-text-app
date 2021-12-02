ec743215d246bd15c809661bb26c8f365dbc4bbc
Hugo Leonardo <hugoleonardo.dev@gmail.com>
2021-12-02 02:43:56 -0300

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,11 @@ import Container from '@mui/material/Container'
 
 import AppHeader from '@components/AppHeader'
 
-
 const Index: React.FC = () => {
     return (
-
-            <Container maxWidth="lg">
-                <AppHeader />
-            </Container>
-
+        <Container maxWidth="lg">
+            <AppHeader />
+        </Container>
     )
 }
 export default Index
